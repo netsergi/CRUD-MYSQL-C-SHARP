62803f2c6952d5344e8b834811e9ccddc0088464
netsergi <netsergi@gmail.com>
2017-09-05 14:30:59 +0200

    primer commit

---
+++ WindowsFormsApp1/db.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace WindowsFormsApp1
+{
+    class db
+    {
+        private void conectar()
+        {
+            private string host;
+            private string usuario;
+            private string password;
+            private string nombredb;
+       
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
